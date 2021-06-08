f48ff6c44f482032497545e7eb7d550a66976325
Mattia Bocchino <pmattiab@live.it>
2021-06-08 17:54:18 +0200

    terminato header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,51 @@
 @extends("layouts.app")
 
 @section("main_content")
-    main
+
+    <section id="comics">
+
+        <div class="container">
+
+            <div class="title">
+                <h1>Current Series</h1>
+            </div>
+
+            <div class="comics_container">
+
+                <ul class="comics_list">
+
+                    @foreach ($comics_array as $comic)
+
+                        <li class="single_comic">
+
+                            <div class="comic_img">
+                                <img src="{{$comic["thumb"]}}" alt="{{$comic["title"]}}">
+                            </div>
+
+                            <div class="comic_title">
+                                <span>{{$comic["series"]}}</span>
+                            </div>
+
+                        </li>
+
+                    @endforeach
+
+                </ul>
+
+            </div>
+
+            <div class="load_more">
+                <button>
+                    LOAD MORE
+                </button>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section id="links">
+
+    </section>
+
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,51 @@
 <header>
-    header
+
+    <div class="top_header">
+        
+        <div class="top_menu">
+            <div class="container">
+                <ul>
+                    <li><a href="#">DC Power Visa</a></li>
+                    <li><a href="#">Additional DC Sites</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="bottom_menu">
+
+                <div class="logo">
+                    <img src="{{asset("img/dc-logo.png")}}" alt="dc comics logo">
+                </div>
+    
+                <div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a class="active" href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Collectibles</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">Fans</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Shop</a></li>
+                        </ul>
+                    </nav>
+                </div>
+    
+                <div class="search">
+                    <input type="text" placeholder="Search">
+                    <span><i class="fas fa-search"></i></span>
+                </div>
+    
+            </div>
+        </div>
+
+    </div>
+
+    <div class="bottom_header">
+    </div>
+
 </header>
